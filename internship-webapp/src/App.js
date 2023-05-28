3bf2bc57e28b644b6f3efd35e38415ab0549c242
elvirkabashi <74553516+elvirkabashi@users.noreply.github.com>
2023-05-28 19:27:55 +0200

    Lastet internships posted

--- internship-webapp/src/App.js
+++ internship-webapp/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import HomeTop from './HomePage/components/HomeTop';
+import InternshipComponent from './HomePage/components/InternshipComponent';
 
 function App() {
   return (
     <div className="App"> 
       <HomeTop/>
+      <InternshipComponent/>
     </div>
   );
 }
---
+++ internship-webapp/src/HomePage/components/InternshipComponent.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+const InternshipComponent = () => {
+  return (
+    <div className="container-fluid my-4" style={{background: "linear-gradient(white, #97EEFF)", padding: "20px"}}>
+      <h3>Lastet internships posted</h3>
+      <p>Find the intern job by the category</p>
+
+      <div className="row justify-content-center align-items-center">
+        <div className="col-md-4 m-2" style={{ backgroundColor: "white", width: "400px",borderRadius: "20px", padding:"25px",boxShadow: "10px 10px 0px grey"}}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img src={require('./../../images/google.png')} alt="Company Logo" style={{ height: "50px", marginRight: "10px",marginBottom: "110px" }} />
+            <div>
+              <b style={{backgroundColor: "#C4F0F9",color:"#00D5FF",borderRadius:"7px", padding:"6px", fontSize: "13px", marginRight:"200px", marginTop: "20px",}}>Full Time</b>
+              <h5 style={{marginRight: "90px",marginTop:"5px"}}>Frontend Developer</h5>
+              <p style={{marginTop:"10px"}}>We are looking for a product designer to join our great team.</p>
+              <button className="btn btn-primary" style={{marginLeft:"160px",marginTop:"5px",backgroundColor:"transparent",border: '2px solid #00D5FF',color:"#00D5FF",fontWeight:"600"}}>Apply Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 m-2" style={{ backgroundColor: "white", width: "400px",borderRadius: "20px", padding:"25px",boxShadow: "10px 10px 0px grey"}}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img src={require('./../../images/google.png')} alt="Company Logo" style={{ height: "50px", marginRight: "10px",marginBottom: "110px" }} />
+            <div>
+              <b style={{backgroundColor: "#C4F0F9",color:"#00D5FF",borderRadius:"7px", padding:"6px", fontSize: "13px", marginRight:"200px", marginTop: "20px",}}>Full Time</b>
+              <h5 style={{marginRight: "90px",marginTop:"5px"}}>Frontend Developer</h5>
+              <p style={{marginTop:"10px"}}>We are looking for a product designer to join our great team.</p>
+              <button className="btn btn-primary" style={{marginLeft:"160px",marginTop:"5px",backgroundColor:"transparent",border: '2px solid #00D5FF',color:"#00D5FF",fontWeight:"600"}}>Apply Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 m-2" style={{ backgroundColor: "white", width: "400px",borderRadius: "20px", padding:"25px",boxShadow: "10px 10px 0px grey"}}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img src={require('./../../images/google.png')} alt="Company Logo" style={{ height: "50px", marginRight: "10px",marginBottom: "110px" }} />
+            <div>
+              <b style={{backgroundColor: "#C4F0F9",color:"#00D5FF",borderRadius:"7px", padding:"6px", fontSize: "13px", marginRight:"200px", marginTop: "20px",}}>Full Time</b>
+              <h5 style={{marginRight: "90px",marginTop:"5px"}}>Frontend Developer</h5>
+              <p style={{marginTop:"10px"}}>We are looking for a product designer to join our great team.</p>
+              <button className="btn btn-primary" style={{marginLeft:"160px",marginTop:"5px",backgroundColor:"transparent",border: '2px solid #00D5FF',color:"#00D5FF",fontWeight:"600"}}>Apply Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 m-2" style={{ backgroundColor: "white", width: "400px",borderRadius: "20px", padding:"25px",boxShadow: "10px 10px 0px grey"}}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img src={require('./../../images/google.png')} alt="Company Logo" style={{ height: "50px", marginRight: "10px",marginBottom: "110px" }} />
+            <div>
+              <b style={{backgroundColor: "#C4F0F9",color:"#00D5FF",borderRadius:"7px", padding:"6px", fontSize: "13px", marginRight:"200px", marginTop: "20px",}}>Full Time</b>
+              <h5 style={{marginRight: "90px",marginTop:"5px"}}>Frontend Developer</h5>
+              <p style={{marginTop:"10px"}}>We are looking for a product designer to join our great team.</p>
+              <button className="btn btn-primary" style={{marginLeft:"160px",marginTop:"5px",backgroundColor:"transparent",border: '2px solid #00D5FF',color:"#00D5FF",fontWeight:"600"}}>Apply Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 m-2" style={{ backgroundColor: "white", width: "400px",borderRadius: "20px", padding:"25px",boxShadow: "10px 10px 0px grey"}}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img src={require('./../../images/google.png')} alt="Company Logo" style={{ height: "50px", marginRight: "10px",marginBottom: "110px" }} />
+            <div>
+              <b style={{backgroundColor: "#C4F0F9",color:"#00D5FF",borderRadius:"7px", padding:"6px", fontSize: "13px", marginRight:"200px", marginTop: "20px",}}>Full Time</b>
+              <h5 style={{marginRight: "90px",marginTop:"5px"}}>Frontend Developer</h5>
+              <p style={{marginTop:"10px"}}>We are looking for a product designer to join our great team.</p>
+              <button className="btn btn-primary" style={{marginLeft:"160px",marginTop:"5px",backgroundColor:"transparent",border: '2px solid #00D5FF',color:"#00D5FF",fontWeight:"600"}}>Apply Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4 m-2" style={{ backgroundColor: "white", width: "400px",borderRadius: "20px", padding:"25px",boxShadow: "10px 10px 0px rgb(122 122 122)"  }}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img src={require('./../../images/google.png')} alt="Company Logo" style={{ height: "50px", marginRight: "10px",marginBottom: "110px" }} />
+            <div>
+              <b style={{backgroundColor: "#C4F0F9",color:"#00D5FF",borderRadius:"7px", padding:"6px", fontSize: "13px", marginRight:"200px", marginTop: "20px",}}>Full Time</b>
+              <h5 style={{marginRight: "90px",marginTop:"5px"}}>Frontend Developer</h5>
+              <p style={{marginTop:"10px"}}>We are looking for a product designer to join our great team.</p>
+              <button className="btn btn-primary" style={{marginLeft:"160px",marginTop:"5px",backgroundColor:"transparent",border: '2px solid #00D5FF',color:"#00D5FF",fontWeight:"600"}}>Apply Now</button>
+            </div>
+          </div>
+        </div>        
+      </div>
+      <div className="row justify-content-center align-items-center">
+        {/* Add more internship divs here */}
+      </div>
+    </div>
+  );
+};
+
+export default InternshipComponent;
